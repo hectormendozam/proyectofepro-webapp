1b1824b7ea4b3f6b5cea69a914615101243a6bbf
alejandroamadorlag <134448417+alejandroamadorlag@users.noreply.github.com>
2023-11-02 14:10:09 -0600

    Login Screen
    Proyecto con configuración inicial

---
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FacadeService } from 'src/app/services/facade.service';
+declare var $:any;
+
+@Component({
+  selector: 'app-login-screen',
+  templateUrl: './login-screen.component.html',
+  styleUrls: ['./login-screen.component.scss']
+})
+export class LoginScreenComponent implements OnInit {
+  //Aquí se definen las variables
+  public type: String = "password";
+  public username: String = "";
+  public password: String = "";
+
+  public errors:any = {};
+
+  constructor(
+    private router: Router,
+    public facadeService: FacadeService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  //Aquí van las funciones de validación
+
+  public login(){
+    //Validar
+    this.errors = [];
+
+    this.errors = this.facadeService.validarLogin(this.username, this.password);
+    if(!$.isEmptyObject(this.errors)){
+      return false;
+    }
+    //Si pasa la validación
+    this.router.navigate(["home"]);
+  }
+
+  public showPassword(){
+    if(this.type == "password"){
+      this.type = "text";
+    }else{
+      this.type = "password";
+    }
+  }
+
+  public goRegistro(){
+    this.router.navigate(["registro"]);
+  }
+
+  public goNewLogin(){
+    this.router.navigate(["registro-producto"]);
+  }
+
+}//Fin clase
